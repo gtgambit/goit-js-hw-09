3f48a74eadbb9b1c7215c6509de1dc6c6d2c0989
gtgambit <107627219+gtgambit@users.noreply.github.com>
2022-11-17 21:28:48 +0000

    Deploying to gh-pages from @ gtgambit/goit-js-hw-09@6a8ddf38f06fcff4efbbfc11e6be7e9dad03c353 🚀

---
+++ 01-color-switcher.846b602a.js
@@ -0,0 +1,2 @@
+const t={startBt:document.querySelector("[data-start]"),stopBt:document.querySelector("[data-stop]"),bodyEl:document.querySelector("body")};let e=null;t.startBt.addEventListener("click",(()=>{e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBt.setAttribute("disabled",""),t.startBt.removeAttribute("disabled")})),t.stopBt.addEventListener("click",(()=>{clearInterval(e),t.stopBt.setAttribute("disabled",""),t.startBt.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.846b602a.js.map
